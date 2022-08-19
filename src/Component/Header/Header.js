0b65376d0bc53ae81aab665c35a83d7cc7ddf960
omarfurkan <omarfurkan92@gmail.com>
2022-08-20 03:52:10 +0600

    service data with section done

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className='bg-[#227db3] text-white font-semibold'>
+        <div className='bg-[#227db3] text-white font-semibold sticky top-0 z-50'>
             <div className=' md:w-11/12 mx-auto'>
-                <div className='grid grid-cols-2 justify-items-start h-12 items-center'>
+                <div className='grid grid-cols-2 justify-items-start h-16 items-center'>
                     <div>logo</div>
                     <div>
                         <Link to='/'> Home</Link>
---
+++ src/Component/Service/Service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+    const { id, courseName, courserDuration, img, pirce, discription } = service;
+    return (
+        <div className='bg-[#76d5d9] '>
+            <div className=''>
+                <img className='h-72 w-96 flex' src={img} alt="" />
+            </div>
+            <div className='text-white px-4 py-2'>
+                <p className='text-xl font-bold py-2'>{courseName}</p>
+                <p className='text-lg fond-semibold'>{discription}</p>
+                <p className=' text-lg py-2'>Course Duration: <span className='font-semibold'>{courserDuration}</span> </p>
+                <p className='text-lg'>Price: $ <span className='text-2xl font-bold'>{pirce}</span> </p>
+                <div className='flex justify-end '>
+                    <button className='border-2 py-1 px-10 font-bold hover:bg-[#45a2a8]'>Buy</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
